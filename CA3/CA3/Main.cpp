9de2d16baff2443e33381eef1682307143ad58ee
KieranK <kierank695@gmail.com>
2016-12-19 02:21:02 +0000

    Implemented Menu
    worked on menuoptions.cpp and added demomenu in main

--- CA3/CA3/Main.cpp
+++ CA3/CA3/Main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include "System.h"
+#include "MenuOptions.h"
+#include <array>
 
 System sys;
 void demo();
+void demoMenu();
 void initialize();
 
 int main()
 {
 	//cout << "Hello World" << endl;
-	initialize();
-	demo();
+	//initialize();
+	//demo();
+	demoMenu();
 	system("pause");
 	return 0;
 }
 
 void initialize() {
-	sys = System();
+	//sys = System();
 }
 void demo() {
 	//System::Register();
@@ -29,4 +33,19 @@ void demo() {
 
 	sys.Logout();
 	//std::cout << sys.current;
+}
+
+void demoMenu()
+{
+	MenuOptions mainmenu("mainmenu");
+	std::array<std::string, 2> menu = { "test", "test" };
+	std::string mainmenuitems[4] = { "LogIn", "Register", "Settings", "Exit" };
+	mainmenu.addAll(mainmenuitems, 4);
+	mainmenu.showMenu();
+	int choice = 0;
+	std::cin >> choice;
+
+	if (choice == menu.size())
+		std::cout << "Exit!!";
+
 }
--- CA3/CA3/MenuOptions.cpp
+++ CA3/CA3/MenuOptions.cpp
@@ -2,31 +2,59 @@
 
 
 
-MenuOptions::MenuOptions(std::string name, int indexOfExitOption)
+
+MenuOptions::MenuOptions(std::string name) : name(name)
 {
 }
 
-MenuOptions::MenuOptions()
+MenuOptions::MenuOptions() : name("default_menu")
 {
 }
-
 
 MenuOptions::~MenuOptions()
 {
 }
 
-int MenuOptions::showMenuGetChoice(std::string strPrompt)
+void MenuOptions::showMenu()
 {
-	return 1;
+	std::cout << "\n" << "***************" << name << "***************" << std::endl;
+
+	int index = 1;
+	
+	for (std::string s : mList)
+	{
+		std::cout << "[" << index << "]" << s << std::endl;
+		index++;
+	}
+
+	std::cout << "Enter Choice: " << std::endl;
 
 }
 
 bool MenuOptions::add(std::string s)
 {
-	return true;
+	if (contains(s) == false)
+	{
+		mList.push_back(s);
+		return true;
+	}
+	else
+		return false;
 }
 
-bool MenuOptions::addAll(std::string sArray[])
+bool MenuOptions::addAll(std::string sArray[], int size)
 {
-	return false;
+	bool success = false;
+	for (int i = 0; i < size; i++)
+	{
+		success = add(sArray[i]);
+	}
+		
+	return success;
+}
+
+bool MenuOptions::contains(std::string checkstring)
+{
+	return (std::find(mList.begin(), mList.end(), checkstring) != mList.end());
+
 }
